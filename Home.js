2bdc38e1810d640e952416b4f2b1c467b6188e4a
한상현 <luck2901@naver.com>
2021-05-01 16:54:07 +0900

    Update_Map1

--- Home.js
+++ Home.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function Home() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text> Hi</Text>
+        <View style={styles.container}>
+            <MapView style={styles.map} />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+});
