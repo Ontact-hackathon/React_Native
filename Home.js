f2b7e5451a8a0f2321ee1be5b6fbfc33a65b8aa3
soosungp33 <soosungp33@gmail.com>
2021-05-02 21:16:03 +0900

    semi finish

--- Home.js
+++ Home.js
@@ -1,28 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import MarkerFactory from './MarkerFactory';
 
+var id =0;
 export default function Home() {
-    const data = [
-        {
-            "id":1,
-            "store": "맛난 호떡",
-            "name": "김우리",
-            "account": "2003004030020",
-            "bank": "국민은행",
-            "latitude": "35.844227",
-            "longitude": "127.127468"
-        }, {
-            "id": 2,
-            "store": "Qwe",
-            "name": "Qwe",
-            "account": "Qwe",
-            "bank": "Qwe",
-            "latitude": "35.848991",
-            "longitude": "127.13656990000001"
-        }
-    ]
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:8080/api/register")
+        .then(response => response.json())
+        .then(data => setData(data))
+    },[])
+
     return (
         <View style={styles.container}>
             <MapView style={styles.map}
@@ -32,13 +21,7 @@ export default function Home() {
                     latitudeDelta: 0.002,
                     longitudeDelta: 0.002,
                 }} >
-                {data.map((obj) => {return (<MarkerFactory key={obj.id} store={obj.store} name={obj.name} account={obj.account} bank={obj.bank} latitude={obj.latitude} longitude={obj.longitude}/>)})}
-                {/* <Marker
-                    coordinate={{ latitude: 35.844015, longitude: 127.127487 }}
-                    image={require('./assets/placeholder2.png')}
-                    title="마싯는 타코야끼"
-                    description="타코야끼 1000원에 5개 드시로오세영"
-                /> */}
+                {data.map((obj) => {return (<MarkerFactory key={id++} store={obj.store} name={obj.name} account={obj.account} bank={obj.bank} latitude={obj.latitude} longitude={obj.longitude}/>)})}
             </MapView>
         </View>
     );
